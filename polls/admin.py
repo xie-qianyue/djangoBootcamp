e0f2f44927955af59ce2ed0fd84321755918b571
xie-qianyue <xie.qianyue.personal@gmail.com>
2014-09-22 17:55:40 +0200

    2014/09/22

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from polls.models import Poll
+from polls.models import Choice
 
 # Register your models here.
-admin.site.register(Poll)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question']}),
+        ('Date information', {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+# admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,7 @@ class Poll(models.Model):
         return self.question
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
